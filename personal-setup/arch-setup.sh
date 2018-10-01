8ae6929593e07eca8ff5142b687439a7c46d9381
Yash-Garg <ben10.yashgarg@gmail.com>
2018-10-01 22:30:43 +0530

    scripts: Add some new scripts and sort them all
    Signed-off-by: Yash-Garg <ben10.yashgarg@gmail.com>

---
+++ personal-setup/arch-setup.sh
@@ -0,0 +1,7 @@
+sudo pacman -Syyu
+sudo pacman -S neofetch firefox filezilla telegram-desktop etcher git mariadb
+
+git config --global user.name "Yash Garg"
+git config --global user.email "ben10.yashgarg@gmail.com"
+
+yaourt -S hyper anydesk sublime-text-dev --noconfirm
---
+++ setup/arch-build-env.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Script to setup an android build environment on Arch Linux and derivative distributions
+
+clear
+echo Installing Dependencies!
+# Update
+sudo pacman -Syyu
+# Install pacaur
+sudo pacman -S base-devel git wget multilib-devel cmake svn clang
+# Install ncurses5-compat-libs, lib32-ncurses5-compat-libs, aosp-devel, xml2, and lineageos-devel
+for p in ncurses5-compat-libs lib32-ncurses5-compat-libs aosp-devel xml2 lineageos-devel; do
+    git clone https://aur.archlinux.org/$p
+    cd $p
+    makepkg -si --skippgpcheck
+    cd -
+    rm -rf $p
+done
+
+echo "All Done :'D"
+echo "Don't forget to run these commands before building, or make sure the python in your PATH is 
+python2 and not python3"
+echo "
+virtualenv2 venv
+source venv/bin/activate"
