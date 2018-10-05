f90af52003731a0956d7f32c34af5b707fb3b5c2
Yash Garg <ben10.yashgarg@gmail.com>
2018-10-05 12:54:55 +0530

    scripts: personal-setup: Add a package in arch-setup.sh

--- personal-setup/arch-setup.sh
+++ personal-setup/arch-setup.sh
@@ -4,4 +4,4 @@ sudo pacman -S neofetch firefox filezilla telegram-desktop etcher git mariadb
 git config --global user.name "Yash Garg"
 git config --global user.email "ben10.yashgarg@gmail.com"
 
-yaourt -S hyper anydesk sublime-text-dev --noconfirm
+yaourt -S hyper anydesk sublime-text-dev spotify --noconfirm
