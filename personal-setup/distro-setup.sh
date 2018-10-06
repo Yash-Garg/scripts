1feb6a11b398e8d19684a93f054759c446def753
Yash-Garg <ben10.yashgarg@gmail.com>
2018-10-07 00:27:56 +0530

    scripts: personal-setup: Revamp script with parameters
    Signed-off-by: Yash-Garg <ben10.yashgarg@gmail.com>

---
+++ personal-setup/distro-setup.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) Yash-Garg <ben10.yashgarg@gmail.com>
+# SPDX-License-Identifier: GPL-v3.0-only
+#
+
+# Prints a formatted header; used for outlining
+function echoText() {
+    RED="\033[01;31m"
+    RST="\033[0m"
+
+    echo -e "${RED}"
+    echo -e "====$( for i in $(seq ${#1}); do echo -e "=\c"; done )===="
+    echo -e "==  ${1}  =="
+    echo -e "====$( for i in $(seq ${#1}); do echo -e "=\c"; done )===="
+    echo -e "${RST}"
+}
+
+function debian_pkgs() {
+    echoText "Installing and updating packages for DEBIAN"
+    sudo apt-get update
+    sudo apt-get upgrade
+}
+
+function arch_pkgs() {
+    echoText "Installing and updating packages for ARCH"
+    sudo pacman -Syyu
+    sudo pacman -S neofetch firefox filezilla telegram-desktop etcher git mariadb gnupg paper-icon-theme
+    yaourt -S hyper anydesk sublime-text-dev spotify flat-remix-git --noconfirm
+}
+
+function gpgkeys() {
+    echoText "Importing GPG Keys"
+    git clone https://github.com/Yash-Garg/GPG_Keys.git ~/gpg_keys
+    gpg --import ~/gpg_keys/ryzenbox_public.asc
+    gpg --import ~/gpg_keys/ryzenbox_private.asc
+}
+
+function git_cfg() {
+    echoText "Configuring git"
+    git config --global user.name "Yash Garg"
+    git config --global user.email "ben10.yashgarg@gmail.com"
+    git config --global signing.key 40C0AECA98D94CBF
+}
+
+# Parameters
+while [[ $# -gt 0 ]]
+do
+param="$1"
+
+case $param in
+     -a|--arch)
+     ARCH="arch"
+     ;;
+     -d|--debian)
+     DEBIAN="debian"
+     ;;
+     -h|--help)
+     echo "Usage: bash distro-setup.sh -a or -d [For arch/debian]"
+     ;;
+esac
+shift
+done
+
+# Define actions on parameters
+
+if [[ "${ARCH}" == "arch" ]]; then
+    arch_pkgs;
+    gpgkeys;
+    git_cfg;
+    echoText "Script succeeded"
+
+elif [[ "${DEBIAN}" == "debian" ]]; then
+    debian_pkgs;
+    gpgkeys;
+    git_cfg;
+    echoText "Script succeeded"
+fi
