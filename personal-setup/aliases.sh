4d7526b76ec3f7aeebc29b46e0cc1e5ab7a20a0b
Yash Garg <ben10.yashgarg@gmail.com>
2018-11-12 20:21:07 +0530

    Create aliases.sh

---
+++ personal-setup/aliases.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) Yash-Garg <ben10.yashgarg@gmail.com>
+# SPDX-License-Identifier: GPL-v3.0-only
+#
+
+alias nfetch='neofetch | lolcat'
+alias zs='source ~/.zshrc'
