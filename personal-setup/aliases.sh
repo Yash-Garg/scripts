d99377fdadab9ff235e86a03eee04bef655d110a
Yash Garg <ben10.yashgarg@gmail.com>
2019-01-29 18:22:36 +0530

    Delete aliases.sh

--- personal-setup/aliases.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Copyright (C) Yash-Garg <ben10.yashgarg@gmail.com>
-# SPDX-License-Identifier: GPL-v3.0-only
-#
-
-alias nfetch='neofetch | lolcat'
-alias zs='source ~/.zshrc'
-alias gitf='git commit --all -s -S'
