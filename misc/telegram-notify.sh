110dd8d4ed15d1426f73076b14285454632525a6
Yash Garg <ben10.yashgarg@gmail.com>
2019-04-06 13:02:49 +0530

    Create telegram-notify.sh

---
+++ misc/telegram-notify.sh
@@ -0,0 +1,15 @@
+# Telegram notify
+notify() {
+	chatid=your_chat_id                         # Your chat id
+	token=your_token_bot                        # Your Bot's token
+	default_message="Hello,I am running perfectly!"
+
+	if [ -z "$@" ]
+	then
+		curl -s --data-urlencode "text=$default_message" "https://api.telegram.org/bot$token/sendMessage?chat_id=$chatid" > /dev/null
+	else
+		curl -s --data-urlencode "text=$@" "https://api.telegram.org/bot$token/sendMessage?chat_id=$chatid" > /dev/null
+	fi
+}
+
+alias notify=notify
